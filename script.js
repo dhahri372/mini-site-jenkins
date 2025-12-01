c420582d5c805ee471b013efe919e8109ee0e46d
dhahri <malekdhahri2001@gmail.com>
2025-12-01 21:59:11 +0100

    Initial commit - Mini site statique

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("helloButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "🎉 Surprise ! Merci d’avoir cliqué, amuse-toi bien sur ce mini-site !";
+});
+
